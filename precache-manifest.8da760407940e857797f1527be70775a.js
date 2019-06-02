2a06722d711336088b7db0401779811450c8db7e
Amir Hamza <hamza_amir@yahoo.com>
2019-06-03 05:47:47 +0600

    Updates

---
+++ precache-manifest.8da760407940e857797f1527be70775a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "701db38576110deb18f3",
+    "url": "/components/static/js/runtime~main.701db385.js"
+  },
+  {
+    "revision": "5538ae18de46dcf3401f",
+    "url": "/components/static/js/main.5538ae18.chunk.js"
+  },
+  {
+    "revision": "347d60cfb1102d6f5dcf",
+    "url": "/components/static/js/1.347d60cf.chunk.js"
+  },
+  {
+    "revision": "e6642c9b4f00ba1337d4ca57e9c33bd5",
+    "url": "/components/index.html"
+  }
+];
